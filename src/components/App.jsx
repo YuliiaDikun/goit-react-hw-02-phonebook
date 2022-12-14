94d5bad2f09d7ff4d81400cef5c42b7f2678cbcc
Yuliia Dikun <dikun.yulia@gmail.com>
2022-12-14 17:08:43 +0200

    add proptyped and styled components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyleComponent } from 'styles/GlobalStyles';
+import {theme} from 'styles/theme'
 export const App = () => {
   return (
-    <div
+    <ThemeProvider theme={theme}>
+      <div
       style={{
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
-    >
-      React homework template
-    </div>
+      >
+        Homework
+      </div>
+    </ThemeProvider>
   );
 };
